222ec89db107e763b23a7b38c61abbeca66f422b
tjdvyzl <tjdvyzl12@naver.com>
2024-05-08 21:44:42 +0900

    전성표 확인했습니다.

---
+++ src/main/java/com/ohgiraffers/meta/jeonseongpyo/Application1.java
@@ -0,0 +1,18 @@
+package com.ohgiraffers.meta.jeonseongpyo;
+
+public class Application1 {
+
+    public static void main(String[] args) {
+
+        /* 정수형 변수 2개를 선언하여  각 20과 30으로 초기화 한 후
+         * 두 수의 더하기, 빼기, 곱하기, 나누기, 나머지를 다음과 같이 출력하세요
+         *
+         * -- 출력 예시 --
+         * 더하기 결과  : 50
+         * 빼기 결과 : -10
+         * 곱하기 결과 : 600
+         * 나누기한 몫 : 0
+         * 나누기한 나머지 : 20
+         * */
+    }
+}
