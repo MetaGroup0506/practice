f49f9c433df3433bfa93ca86623857141c493dcb
Yohan Park <pyh95510@gmail.com>
2024-05-08 07:57:36 -0500

    practice/pull request

--- src/main/java/com/ohgiraffers/meta/yohanpark/Application1.java
+++ src/main/java/com/ohgiraffers/meta/yohanpark/Application1.java
@@ -1,4 +1,4 @@
-package com.ohgiraffers.meta;
+package com.ohgiraffers.meta.yohanpark;
 
 public class Application1 {
 
@@ -14,5 +14,11 @@ public class Application1 {
          * 나누기한 몫 : 0
          * 나누기한 나머지 : 20
          * */
+        int num1=20, num2=30;
+        System.out.println("더하기 결과 : " + (num1+num2));
+        System.out.println("빼기 결과 : " + (num1-num2));
+        System.out.println("곱하기 결과 : " + (num1*num2));
+        System.out.println("나누기한 몫 : " + (num1/num2));
+        System.out.println("나누기한 나머지 : " + (num1%num2));
     }
 }
