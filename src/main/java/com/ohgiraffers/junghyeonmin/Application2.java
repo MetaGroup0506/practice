1c6b23cac35b87f4efe8815f47a0311256424bfb
JungHyeonmin <2ee42a45@gmail.com>
2024-05-10 20:44:40 +0900

    테스트 메세지입니다.

---
+++ src/main/java/com/ohgiraffers/junghyeonmin/Application2.java
@@ -0,0 +1,41 @@
+package com.ohgiraffers.junghyeonmin;
+
+public class Application2 {
+
+    public static void main(String[] args) {
+
+        /* 계산기 프로그래밍 */
+        checkMethod();
+    }
+
+
+    // 반환값 : void
+    // 메소드명 : checkMethod
+    // 호출하면 "메소드 호출 확인"을 출력하는 기능의 메소드
+    public static void checkMethod() {
+        System.out.println("메소드 호출 확인");
+    }
+
+    // 반환값 : String
+    // 메소드명 : sumTwoNumber
+    // 두개의 전달인자를 받아 두수의 합을 반환
+    // 출력의 내용은 "10과 20의 합은 30입니다." 라는 문장을
+    // 출력하는 기능
+
+    public static String sumTwoNumber(int num1, int num2) {
+        System.out.println("10과 20의 합은 30입니다.");
+        String result = "" + (num1 + num2);
+        return result;
+    }
+
+    // 반환값 : int
+    // 메소드명 : multiTwoNumber
+    // 두개의 전달인자를 받아 두수의 곱을 반환
+    // 출력의 내용은 "10과 20의 곱은 200입니다." 라는 문장을
+    // 출력하는 기능
+    public static int multiTwoNumber(int num1, int num2) {
+        int result = num1 * num2;
+        System.out.println("10과 20의 곱은 200입니다.");
+        return result;
+    }
+}
