51aa908c6d8541ce4d646a84ee449ba1c1c846d0
leebc007 <leebc007@naver.com>
2024-05-10 20:42:28 +0900

    이동렬 제출

--- src/main/java/com/ohgiraffers/meta/Application2.java
+++ src/main/java/com/ohgiraffers/meta/Application2.java
@@ -6,8 +6,11 @@ public class Application2 {
 
         /* 계산기 프로그래밍 */
         checkMethod();
+        sumTwoNumber(10, 20);
+        multiTwoNumber(10, 20);
     }
 
+   
 
     // 반환값 : void
     // 메소드명 : checkMethod
@@ -22,6 +25,15 @@ public class Application2 {
     // 두개의 전달인자를 받아 두수의 합을 반환
     // 출력의 내용은 "10과 20의 합은 30입니다." 라는 문장을
     // 출력하는 기능
+    public static String sumTwoNumber(int a, int b) {
+        int c = a + b;
+        String str = a + " 과 " + b + " 의 합은 " + c + " 입니다.";
+
+        System.out.println(str);
+
+        return str;
+
+    }
 
 
     // 반환값 : int
@@ -29,4 +41,16 @@ public class Application2 {
     // 두개의 전달인자를 받아 두수의 곱을 반환
     // 출력의 내용은 "10과 20의 곱은 200입니다." 라는 문장을
     // 출력하는 기능
+    public static String multiTwoNumber(int num1, int num2){
+        int num3 = num1 * num2;
+        String str2 = num1 + " 과 " + num2 + " 의 곱은 " + num3 +" 입니다. ";
+
+        System.out.println(str2);
+
+        return str2;
+
+    }
+
+
+
 }
