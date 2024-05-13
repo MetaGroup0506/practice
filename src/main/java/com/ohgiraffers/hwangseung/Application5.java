dacfa97a14daa03dfc3b7c4fa5d94a90f0de3e83
ft6066 <2018066@naver.com>
2024-05-14 00:22:07 +0900

    과제 3~6 제출입니다.

---
+++ src/main/java/com/ohgiraffers/hwangseung/Application5.java
@@ -0,0 +1,44 @@
+package com.ohgiraffers.hwangseung;
+
+import java.util.Scanner;
+
+public class Application5 {
+
+    public static void main(String[] args) {
+
+        /* 문자열을 하나 입력받아 문자 자료형 배열로 바꾼 뒤
+         * 검색할 문자를 하나 입력 받아
+         * 입력 받은 검색할 문자가 문자열에 몇개 있는지를 출력하세요
+         *
+         * -- 입력 예시 --
+         * 문자열을 하나 입력하세요 : helloworld
+         *
+         * -- 출력 예시 --
+         * 입력하신 문자열 helloworld에서 찾으시는 문자 l은 3개 입니다.
+         * */
+
+        Scanner sc = new Scanner(System.in);
+        Scanner sc2 = new Scanner(System.in);
+
+        System.out.print("문자열을 하나 입력하세요 : ");
+        String str = sc.nextLine();
+
+        char[] ch = str.toCharArray(); //문자열을 문자 배열로 변환
+
+        System.out.print("검색할 문자를 입력하세요 : ");
+        char str2 = sc.nextLine().charAt(0);
+
+
+
+        int count = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+
+                if(ch[i] == str2){
+                    count++;
+                }
+            }
+        System.out.println("입력하신 문자열 " + str + "에서 찾으시는 문자 " + str2 +"은(는) " + count + "개 입니다.");
+        }
+    }
+
