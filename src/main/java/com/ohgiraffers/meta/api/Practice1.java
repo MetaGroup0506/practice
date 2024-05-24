4e93bea2feea5e6223623f36cf7d1f20493e98b5
seungje-yu <coffit23@gmail.com>
2024-05-24 21:29:49 +0900

    Feat: 수업자료 업로드 & 실습문제 업로드

---
+++ src/main/java/com/ohgiraffers/meta/api/Practice1.java
@@ -0,0 +1,24 @@
+package com.ohgiraffers.meta.api;
+
+public class Practice1 {
+
+    public static void main(String[] args) {
+
+        Practice1 p1 = new Practice1();
+//        String result = sp.preChar("hello world");
+//        System.out.println(result);
+//
+//        int num = p1.charSu("application car cable", 'c');
+//        System.out.println(num);
+    }
+
+    // +preChar(String s) :String :> 문자열을 넘겨받아, 첫글자를 대문자로 바꾸는 메서드 작성
+    // +charSu(String s, char ch):int :> 문자열에서 찾는 문자가 몇 개 포함되어 있는지 반환하는 메서드 작성
+
+    // 출력예시
+    /*
+    *
+    *    Hello world
+    *    3
+    * */
+}
