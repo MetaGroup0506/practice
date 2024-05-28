04827e4ce51330708c2a3eed935014ae2a0f825b
gns2862 <gns2862@naver.com>
2024-05-28 19:49:45 +0900

    test

---
+++ src/main/java/com/ohgiraffers/kimjonghoon/member/MemberDTO.java
@@ -0,0 +1,71 @@
+package com.ohgiraffers.kimjonghoon.member;
+
+public class MemberDTO {
+    private String id;
+    private String pwd;
+    private String name;
+    private int age;
+    private char gender;
+    private String phone;
+    private String email;
+
+    public MemberDTO() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
