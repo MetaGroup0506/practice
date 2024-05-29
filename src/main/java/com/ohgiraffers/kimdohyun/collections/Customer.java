320dc726bea1e0df8bed0af2d564ea7b51d2f265
syamcat <kdh1998kdh@gmail.com>
2024-05-29 15:21:28 +0900

    5.27 Collection, Generic Subject

---
+++ src/main/java/com/ohgiraffers/kimdohyun/collections/Customer.java
@@ -0,0 +1,42 @@
+package com.ohgiraffers.kimdohyun.collections;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Collections;
+import java.util.Arrays;
+
+/*
+ *   Customer.java
+ *   Author : syamcat
+ *   Created : 24. 5. 29.
+ **/
+public class Customer {
+    private List<String[]> customers = new ArrayList<>();
+
+    public void addCustomer(String customerData){
+        String[] data = customerData.split(",");
+        this.customers.add(data);
+    }
+
+    public void addCustomer(int idx, String customerData){
+        String[] data = customerData.split(",");
+        this.customers.add(idx, data);
+    }
+
+    public void sortCustomers(int idx) {
+        if (idx != 0) {
+            customers.sort((a, b) -> a[0].compareTo(b[0]));
+        }
+        customers.sort((a, b) -> a[idx].compareTo(b[idx]));
+    }
+
+    public void printList() {   // Print Customers List
+        System.out.println("=-=-=-=-=-=-=-=-Customers List=-=-=-=-=-=-=-=");
+        for (String[] data : customers) {
+            System.out.println(Arrays.toString(data));
+        }
+        System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+    }
+
+}
