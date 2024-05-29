5871434f83a5fbf5cdb2d21aae7ad36c687471ed
lxxsxynnn <blip0819@gmail.com>
2024-05-29 21:06:43 +0900

    임소연 map 과제 제출합니다.

---
+++ src/main/java/com/ohgiraffers/soyeonlim/member/Member.java
@@ -0,0 +1,68 @@
+package com.ohgiraffers.soyeonlim.member;
+
+public class Member {
+    private String id;
+    private String password;
+    private String name;
+    private int age;
+    private String phone;
+
+    @Override
+    public String toString() {
+        return "Member{" +
+                "id='" + id + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Member(String id, String password, String name, int age, String phone) {
+        this.id = id;
+        this.password = password;
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+    }
+}
