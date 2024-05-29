d3e562a46d78616018fecc2964cb36676779ade0
kgpwls7357 <kgpwls7357@gmail.com>
2024-05-29 15:46:27 +0900

    김혜진(7357) 5/27 과제 제출합니다!

---
+++ src/main/java/com/ohgiraffers/hyejinkim/collection/Customer.java
@@ -0,0 +1,41 @@
+package com.ohgiraffers.hyejinkim.collection;
+
+public class Customer {
+    private String name;
+    private int age;
+    private String gender;
+    private double points;
+
+    public Customer(String name, int age, String gender, double points) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.points = points;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public double getPoints() {
+        return points;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender='" + gender + '\'' +
+                ", points=" + points +
+                '}';
+    }
+}
