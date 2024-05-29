3707a4ad36754fc8463ffc87cee5e7333bf65b06
kgpwls7357 <kgpwls7357@gmail.com>
2024-05-29 15:49:37 +0900

    김혜진(7357) 5/27 과제 제출합니다!

--- src/main/java/com/ohgiraffers/hyejinkim/collection/Practice1.java
+++ src/main/java/com/ohgiraffers/hyejinkim/collection/Practice1.java
@@ -31,13 +31,11 @@ public class Practice1 {
     public static void main(String[] args) {
 
         List<Customer> customers = new ArrayList<>();
+        customers.add(0, new Customer("이황", 66, "남", 9999.9));
         customers.add(new Customer("홍길동", 25, "남", 1250.5));
         customers.add(new Customer("박문수", 33, "남", 3457.8));
         customers.add(new Customer("김춘추", 38, "남", 2485.6));
         customers.add(new Customer("신사임당", 43, "여", 2300.9));
-
-
-        customers.add(0, new Customer("이황", 66, "남", 9999.9));
 
 
         customers.sort(Comparator.comparing(Customer::getName));
