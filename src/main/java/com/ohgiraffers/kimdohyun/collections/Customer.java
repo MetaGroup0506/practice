9c47accfdd4837aa08807b35a7749e600f588012
syamcat <kdh1998kdh@gmail.com>
2024-05-29 15:34:07 +0900

    remove imports

--- src/main/java/com/ohgiraffers/kimdohyun/collections/Customer.java
+++ src/main/java/com/ohgiraffers/kimdohyun/collections/Customer.java
@@ -1,9 +1,7 @@
 package com.ohgiraffers.kimdohyun.collections;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Collections;
 import java.util.Arrays;
 
 /*
