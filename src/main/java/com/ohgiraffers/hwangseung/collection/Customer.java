e2307f27e5b53e8144a7f37b6203f056b3887403
ft6066 <2018066@naver.com>
2024-05-30 23:13:28 +0900

    황세웅 5월 27일, 29일 과제 제출입니다.

---
+++ src/main/java/com/ohgiraffers/hwangseung/collection/Customer.java
@@ -0,0 +1,62 @@
+package com.ohgiraffers.hwangseung.collection;
+
+public class Customer {
+
+    private String name;
+    private int age;
+    private char gender;
+    private double point;
+
+
+    public Customer() {
+    }
+
+    public Customer(String name, int age,  char gender, double point) {
+        this.age = age;
+        this.name = name;
+        this.gender = gender;
+        this.point = point;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public double getPoint() {
+        return point;
+    }
+
+    public void setPoint(double point) {
+        this.point = point;
+    }
+
+    @Override
+    public String toString() {
+        return "\n[" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                ", point=" + point +
+                ']' + "\n";
+    }
+}
